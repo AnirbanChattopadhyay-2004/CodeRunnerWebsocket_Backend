d9eacb7d90a9528fa90e6925b847ede00ca6dde8
AnirbanChattopadhyay-2004 <anirban.chtt@gmail.com>
2025-02-18 02:42:03 +0530

    Added the permanent sync logic in collab

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+const { createClient } = require("redis")
 const express = require('express');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 const cors = require("cors")
+
+
 const app = express();
 app.use(cors())
 const server = createServer(app);
 require('dotenv/config')
 const url = process.env.url
+const client = createClient ({
+  url : process.env.redis_url,
+  socket: {
+    reconnectStrategy: (retries) => Math.min(retries * 50, 2000), // Exponential backoff
+  },
 
+});
+async function connection(){
+  await client.connect()
+  client.on("error",(err)=>{console.log("Redis err",err)})
+}
+connection()
+let room_val = {}
 const io = new Server(server, {
  cors:{
-  origin:url || "http://localhost:5173"
+  origin:"http://localhost:5173"
  }
 }); 
 io.on('connection', (socket) => {
@@ -22,15 +37,30 @@ io.on('connection', (socket) => {
   socket.on('collab code',  (data) => { //listening to chat message event
     
     // include the offset with the message
-    // console.log(data)
+    // console.log(data.code)
+    room_val[data.room] = (data.code)
     socket.to(data.room).emit('receive code', data.code); // those user are there in the room where this client who sent the code is there, send them all the message.
   });
-  socket.on("join room",(data)=>{
-    console.log(data)
+  socket.on("join room",async (data)=>{
+    // console.log(data)
+    // console.log("joined "+room_val[data])
     socket.join(data)
+    // console.log("client data"+await client.get(data))
+    const val = await client.get(data)
+    if(!room_val[data])
+      room_val[data] = JSON.stringify(val) === "{}"?"":val
+    socket.emit("receive code",room_val[data]?room_val[data]:"")
   })
 });
 
-server.listen(3001, () => {
+setInterval(()=>{
+  Object.keys(room_val).forEach(async (key)=>{
+    console.log(key)
+    await client.set(key,room_val[key])
+  })
+},100000)
+
+server.listen(3001, () => { 
   console.log('server running at http://localhost:3001');
 });
+ 
